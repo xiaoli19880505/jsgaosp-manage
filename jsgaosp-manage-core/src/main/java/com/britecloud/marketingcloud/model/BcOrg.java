5d82f1fd357391853e2f7bc16f1e35289909d15e
hudasen <hudasen>
2019-04-18 15:36:32 +0800

    组织机构Modal

---
+++ jsgaosp-manage-core/src/main/java/com/britecloud/marketingcloud/model/BcOrg.java
@@ -0,0 +1,19 @@
+package com.britecloud.marketingcloud.model;
+
+import lombok.Data;
+
+@Data
+public class BcOrg {
+
+    private String orgNo;
+    private String orgName;
+    private String pOrgNo;
+    private String status;
+    private String orgType;
+    private String memo;
+    private String chargePerson;
+    private String officeTel;
+    private String mobileTel;
+    private String address;
+
+}
