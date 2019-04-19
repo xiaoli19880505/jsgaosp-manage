865b8cf8d69aa1a1aa31a2a61e8ea557c39ad72c
hudasen <hudasen>
2019-04-19 15:17:05 +0800

    增加Easyui

---
+++ jsgaosp-manage-web/src/main/webapp/assets/bs-datetimepicker/daterangepicker.zh-CN.js
@@ -0,0 +1,14 @@
+(function($) {
+	$.fn.daterangepicker.locale = {
+		applyLabel : "确定",
+		cancelLabel : "取消",
+		fromLabel : "开始",
+		toLabel : "结束",
+		weekLabel : "W",
+		customRangeLabel : "自定义范围",
+		daysOfWeek : moment.weekdaysMin(),
+		monthNames : [ '一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月',
+				'十月', '十一月', '十二月' ],
+		firstDay : 0
+	};
+}(jQuery));
