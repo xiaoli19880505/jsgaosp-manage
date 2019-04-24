0eac52d69a1f88e7066f372ed84f54cef31b661f
loulianlian <loulianlian123456>
2019-04-24 09:45:52 +0800

    登录那块把数据字典放到session里，ctrl里直接从session里取

--- jsgaosp-manage-web/src/main/webapp/app/js/app/login/login-ctrl.js
+++ jsgaosp-manage-web/src/main/webapp/app/js/app/login/login-ctrl.js
@@ -3,8 +3,8 @@
 /* Controllers */
 // signin controller
 app.controller('LoginController', ['$scope', '$http', '$state',
-    '$window','$sessionStorage', 'GG', 'loginServ',
-    function ($scope, $http, $state, $window,$sessionStorage, GG, loginServ) {
+    '$window','$sessionStorage', 'GG', 'loginServ','commonServ',
+    function ($scope, $http, $state, $window,$sessionStorage, GG, loginServ,commonServ) {
         var homeState = GG.states.home;
         $scope.user = {};
         $scope.authError = null;
@@ -16,8 +16,22 @@ app.controller('LoginController', ['$scope', '$http', '$state',
                 if (!data.auth) {
                     $scope.authError = '帐号或密码错误!';
                 } else {
-                	
-                	$sessionStorage.user = data.user;
+
+                   // 获取数据字典列表
+                    var  codeKeyList=['sys_type','xz_type','yw_type'];
+                    var  codeList={};
+                    angular.forEach(codeKeyList,function (item) {
+                        commonServ.getCodeListByColName(item).then(function(data){
+                            codeList[item]=data.data;
+                        });
+                    })
+                    $sessionStorage.codeList=codeList;
+
+
+
+
+
+                    $sessionStorage.user = data.user;
                     $sessionStorage.company = data.company;
                     $sessionStorage.role = data.role;
                 	
